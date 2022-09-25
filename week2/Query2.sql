5072bdb789c509876967caab119d86c790d71c4e
Placidusali@gmail.com <placidusali@gmail.com>
2022-09-25 18:26:38 +0100

    added new files

---
+++ week2/Query2.sql
@@ -0,0 +1,8 @@
+-- SQLite
+
+/* Write a query for paid courses, showing the course with the 
+highest sales per month */
+SELECT course_id, num_reviews
+FROM udemy_courses
+WHERE is_paid = TRUE AND num_reviews != 0 
+ORDER BY num_reviews DESC;
